de5335df03fb65ec3ab2acb8cd0ec8ea3ee224e8
Henrique Lopes <riquellopes@gmail.com>
2012-07-01 15:15:00 -0300

    update file settings, on branch shop

--- settings.py
+++ settings.py
@@ -31,10 +31,11 @@ META_GOOGLE_SITE_VERIFICATION=environ.get('GOOGLE_SITE_VERIFICATION')
 ##############################
 # Email:				 	 #
 ##############################
+MAIL='contato@henriquelopes.com.br'
 MAIL_CODE='&#099;&#111;&#110;&#116;&#097;&#116;&#111;&#064;&#104;&#101;&#110;&#114;&#105;&#113;&#117;&#101;&#108;&#111;&#112;&#101;&#115;&#046;&#099;&#111;&#109;&#046;&#098;&#114;' 
 
 ##############################
-# Secreat:				 	 #
+# Secret:				 	 #
 ##############################
 CONSUMER_SECRET=environ.get('CONSUMER_SECRET')
 CONSUMER_KEY=environ.get('CONSUMER_KEY')
