5e409269e714166adca506914577776cc470f188
Henrique Lopes <riquellopes@gmail.com>
2016-02-24 23:33:36 -0300

    rename file and flake8

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object("settings")
+app.config.from_object("config")
 
 @app.route("/")
 def home():
---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+FREEZER_DESTINATION = "site"
+TEST = environ.get("TEST", False)
+MAIL_CODE = '&#099;&#111;&#110;&#116;&#097;&#116;&#111;&#064;&#104;&#101;&#110;&#114;&#105;&#113;&#117;&#101;&#108;&#111;&#112;&#101;&#115;&#046;&#099;&#111;&#109;&#046;&#098;&#114;'
+CONSUMER_KEY = environ.get("CONSUMER_KEY")
+CONSUMER_SECRET = environ.get("CONSUMER_SECRET")
