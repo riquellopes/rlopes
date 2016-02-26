b187fa6f021d04890a9b3b1cc51bdeccf6e8e4e4
Henrique Lopes <riquellopes@gmail.com>
2016-02-25 22:38:23 -0300

    add description

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 from flask_frozen import Freezer
+from flask.ext.cache import Cache
 
 app = Flask(__name__)
 app.config.from_object("config")
 freezer = Freezer(app)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 @app.route("/")
+@cache.cached(timeout=1800)
 def home():
     """
         Get first page.
